4c383f7a90d17df0ed616454eca115f1558d7854
DeokKeun Song <htans@naver.com>
2020-11-05 18:51:26 +0900

    Commit

---
+++ Chapter_2/2_1.java
@@ -0,0 +1,10 @@
+class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println(3+5);    // 화면에 8을 출력
+        System.out.println("3+5");  // 화면에 3+5를 출력
+
+        // System.out.print();
+        System.out.println();
+    }
+}
